128035941947a2673f2d9480c11702413659a629
Dương Quý Vinh <Vinh@gmail.com>
2022-09-07 00:05:25 +0700

    fix main layout

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -1,21 +1,20 @@
-import { Col, Row } from "antd";
+import classNames from "classnames/bind";
 import { SideBar } from "~/components";
+import styles from "./MainLayout.module.scss";
 
-
+const cx = classNames.bind(styles);
 interface propsTypes {
   children: JSX.Element;
 }
 
 const MainLayout: React.FC<propsTypes> = ({ children }) => {
   return (
-    <Row style={{height: '100vh'}}>
-      <Col span={5} style={{height: '100%'}}>
+    <div className={cx("main-layout-container")}>
+      <div className={cx("sidebar")}>
         <SideBar />
-      </Col>
-      <Col span={19}>
-        {children}
-      </Col>
-    </Row>
+      </div>
+      <div className={cx("content")}>{children}</div>
+    </div>
   );
 };
 
