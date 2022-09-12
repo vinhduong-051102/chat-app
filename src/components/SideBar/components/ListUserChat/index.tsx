e0a834986563d851bc8e85c78351a9a6e78fb7aa
Dương Quý Vinh <Vinh@gmail.com>
2022-09-12 23:09:47 +0700

    add display time

--- src/components/SideBar/components/ListUserChat/index.tsx
+++ src/components/SideBar/components/ListUserChat/index.tsx
@@ -2,10 +2,10 @@ import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Avatar, Col, Row, Space } from "antd";
 import classNames from "classnames/bind";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { dotdotdot } from "~/helper";
 import styles from "./ListUserChat.module.scss";
-import { useState } from "react";
-import { Link } from "react-router-dom";
 
 const cx = classNames.bind(styles);
 
@@ -24,11 +24,32 @@ interface propsTypes {
 }
 
 const ListUserChat: React.FC<propsTypes> = ({ data }) => {
+  const [hours, setHours] = useState("");
+  const [minutes, setMinutes] = useState("");
+
   const [isActive, setIsActive] = useState(-1);
   const handleSelectChatRoom = (index: number) => {
     setIsActive(index);
   };
-  
+  useEffect(() => {
+    if (data.length > 0) {
+      const createdAt = data[0].createdAt;
+      const hours = new Date(createdAt).getHours();
+      const minutes = new Date(createdAt).getMinutes();
+      setHours(() => {
+        if(hours < 10) {
+          return `0${hours}`
+        }
+        return String(hours)
+      });
+      setMinutes(() => {
+        if(minutes < 10) {
+          return `0${minutes}`
+        }
+        return String(minutes)
+      });
+    }
+  }, [data]);
   return (
     <div style={{ width: "100%" }} className={cx("wrapper")}>
       <Row>
@@ -61,7 +82,7 @@ const ListUserChat: React.FC<propsTypes> = ({ data }) => {
                       <Col span={24}>
                         <div className={cx("user-name")}>{item.displayName}</div>
                         <div className={cx("latest-message")}>
-                          {dotdotdot(item.latestMessage, 14)} 12:09
+                          {dotdotdot(item.latestMessage, 14)} {`${hours}:${minutes}`}
                         </div>
                       </Col>
                     </Space>
