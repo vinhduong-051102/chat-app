ecab451fe2040fb2c15f2fc6184d167b08cac1df
Dương Quý Vinh <Vinh@gmail.com>
2022-09-12 23:17:04 +0700

    fix bug time

--- src/components/SideBar/components/ListUserChat/index.tsx
+++ src/components/SideBar/components/ListUserChat/index.tsx
@@ -2,7 +2,7 @@ import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Avatar, Col, Row, Space } from "antd";
 import classNames from "classnames/bind";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { Link } from "react-router-dom";
 import { dotdotdot } from "~/helper";
 import styles from "./ListUserChat.module.scss";
@@ -24,32 +24,36 @@ interface propsTypes {
 }
 
 const ListUserChat: React.FC<propsTypes> = ({ data }) => {
-  const [hours, setHours] = useState("");
-  const [minutes, setMinutes] = useState("");
-
   const [isActive, setIsActive] = useState(-1);
   const handleSelectChatRoom = (index: number) => {
     setIsActive(index);
   };
-  useEffect(() => {
-    if (data.length > 0) {
-      const createdAt = data[0].createdAt;
-      const hours = new Date(createdAt).getHours();
-      const minutes = new Date(createdAt).getMinutes();
-      setHours(() => {
-        if(hours < 10) {
-          return `0${hours}`
+  const transformDisplayHours = useCallback(
+    (index: number) => {
+      if (data.length > 0) {
+        const createdAt = data[index].createdAt;
+        const hours = new Date(createdAt).getHours();
+        if (hours < 10) {
+          return `0${hours}`;
         }
-        return String(hours)
-      });
-      setMinutes(() => {
-        if(minutes < 10) {
-          return `0${minutes}`
+        return String(hours);
+      }
+    },
+    [data]
+  );
+  const transformDisplayMinutes = useCallback(
+    (index: number) => {
+      if (data.length > 0) {
+        const createdAt = data[index].createdAt;
+        const minutes = new Date(createdAt).getMinutes();
+        if (minutes < 10) {
+          return `0${minutes}`;
         }
-        return String(minutes)
-      });
-    }
-  }, [data]);
+        return String(minutes);
+      }
+    },
+    [data]
+  );
   return (
     <div style={{ width: "100%" }} className={cx("wrapper")}>
       <Row>
@@ -82,7 +86,7 @@ const ListUserChat: React.FC<propsTypes> = ({ data }) => {
                       <Col span={24}>
                         <div className={cx("user-name")}>{item.displayName}</div>
                         <div className={cx("latest-message")}>
-                          {dotdotdot(item.latestMessage, 14)} {`${hours}:${minutes}`}
+                          {dotdotdot(item.latestMessage, 14)} {`${transformDisplayHours(index)}:${transformDisplayMinutes(index)}`}
                         </div>
                       </Col>
                     </Space>
